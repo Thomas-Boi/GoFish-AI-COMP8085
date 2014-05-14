4cd570c6ddd1eded853e42fb81f9bf8ef0afb1dd
Leah Krynitsky <leah8@vt.edu>
2014-05-14 04:36:29 -0400

    added comments

--- cards.py
+++ cards.py
@@ -1,52 +1,58 @@
 import random
 
+#class to create a deck of cards
 class Cards:
 	"""A standard deck of cards"""
 	
-	def __init__(self):
-		self.deck = []
-		self.comphand = []
-		self.compsets = []
-		self.userhand = []
-		self.usersets = []
+	def __init__(self):	#create 
+		self.deck = []		#current deck
+		self.comphand = []	#computers hand
+		self.compsets = []	#computers made sets (4 of a kind)
+		self.userhand = []	#users hand
+		self.usersets = []	#users made sets
 
-		for x in range(2, 15):
-			for y in range(1, 5):
-				if x == 11:
+		#add 4 of each card to the deck
+		for x in range(2, 15):	#each type of card (2-10, J, Q, K, A)
+			for y in range(1, 5):	#4 of each type in a deck
+				if x == 11:	#jack
 					self.deck.append('J')
-				elif x == 12:
+				elif x == 12:	#queen
 					self.deck.append('Q')
-				elif x == 13:
+				elif x == 13:	#king
 					self.deck.append('K')
-				elif x == 14:
+				elif x == 14:	#ace
 					self.deck.append('A')
-				else:
+				else:		#2-10
 					self.deck.append(x)
-		
+
+		#select 7 random cards from the deck for the computer
 		for x in range(0, 7):
 			card = random.choice(self.deck)
 			self.comphand.append(card)
 			self.deck.remove(card)
-
+		
+		#select 7 random cards from the deck for the user
 		for x in range(0, 7):
 			card = random.choice(self.deck)
 			self.userhand.append(card)
 			self.deck.remove(card)
 	
+	#remove 'card' from hand of current 'turn'
 	def remove(self, turn, card):
 		if turn == 'user':
 			self.userhand.remove(card)
 		else:
 			self.comphand.remove(card)
 
+	#append 'card' to hand of current 'turn'
 	def append(self, turn, card):
 		if turn == 'user': 
 			self.userhand.append(card)
 		else:
 			self.comphand.append(card)
 	
+	#print users current hand in order from 2 to Ace
 	def printHand(self):
-		temp = []
 		user_str = " "
 		for x in range(2, 15):
 			if x == 11:
@@ -69,11 +75,11 @@ class Cards:
 				user_str = user_str + curr + ", "
 				count = count - 1
 
-		#user_str = str(self.userhand).strip('[]')
 		sets_str = str(self.usersets).strip('[]')
 		print("Your Hand: " + user_str.replace("'", ""))
 		print("Your Sets: " + sets_str.replace("'", ""))
 	  
+	#draw a random card from the deck on a wrong guess
 	def drawCard(self, player):
 		card = random.choice(self.deck)
 		self.deck.remove(card)
@@ -82,6 +88,7 @@ class Cards:
 		else:
 			self.userhand.append(card)
 			
+	#check if a set has been made
 	def checkForSet(self, player):
 		if player == "user":
 			for c in self.userhand:
@@ -97,7 +104,9 @@ class Cards:
 					for x in range(0, 4):
 						self.comphand.remove(c)
 					self.compsets.append(c)
-
+	
+	#print computer sets - removed call to this function as suggested in peer review
+	#but left the function here so it could be re-implemented if the player desired
 	def printComp(self):
 		comp_str = str(self.compsets).strip('[]')
 		print("My Sets: " + comp_str.replace("'", ""))
--- gofish.py
+++ gofish.py
@@ -13,33 +13,36 @@ numStrings = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '
 faceStrings = ['King', 'Queen', 'Jack', 'Ace']
 faceCard = 0
 
+#Determine the type of card 
 def processInput(user_input):
 	cardType = -1
 	try:
-		if ' ' in user_input: 
-			cardType = -2
+		if ' ' in user_input:
+			cardType = -2	#invalid format
 		elif user_input.upper() in (card.upper() for card in faceCards):
-			cardType = 1
+			cardType = 1	#face card
 		elif user_input.upper() in (card.upper() for card in faceStrings):
-			cardType = 1
+			cardType = 1	#face card
 		elif user_input.upper() in (card.upper() for card in numStrings):
-			cardType = 0
+			cardType = 0	#number card
 		elif int(user_input) in regCards:
-			cardType = 0
+			cardType = 0	#number card
 		return cardType
-	except ValueError:
-		cardType = -2;
+	except ValueError:	#catch exceptions 
+		cardType = -2;	#invalid format
 		return cardType
 
+#Return either a number (2-10) or J, Q, K, A for display
 def adjustCard(cardType, user_input):
-	if cardType == 0:
+	if cardType == 0:	#number card - return int value
 		if user_input.upper() in (card.upper() for card in numStrings):
 			return int(regCards[numStrings.index(user_input)])
 		else:
 			return int(user_input)
-	else:
+	else:	#face card - return first letter (K, Q, J, A)
 		return user_input[0].upper()
 
+#Handle an incorrect guess
 def guessWrong(turn):
 	deck.drawCard(turn)
 	deck.checkForSet(turn)
@@ -50,18 +53,19 @@ def guessWrong(turn):
 	else:
 		print("Went Fishing...Your Turn!")
 		
-	time.sleep(1)
+	time.sleep(1)	#Delay 1 second for user to view output
 		
+#Handle a correct guess
 def guessRight(turn, card, count, cardType):
-	if cardType == 0:
+	if cardType == 0:	#number card
 		index = regCards.index(card)
-		cardString = numStrings[index]
-	else:
+		cardString = numStrings[index]	#word version of number
+	else:			#face card
 		index = faceCards.index(card)
-		cardString = faceStrings[index]
+		cardString = faceStrings[index]	#word version of face card
 
 	if count > 1:
-		cardString = cardString + "s"
+		cardString = cardString + "s"	#add s if there were multiples
 	
 	if turn == 'user':
 		print("I had " + str(count) + " " + cardString)
@@ -69,60 +73,63 @@ def guessRight(turn, card, count, cardType):
 	else:
 		print("Thanks for the " + cardString)
 
-	updateHand(count, card, turn)
+	updateHand(count, card, turn)		#update user and comp hands
 
+#Update user and comp hands after a correct guess
 def updateHand(count, card, turn):
-	if turn == 'user':
+	if turn == 'user':	#set the opponent
 		opp = 'comp'
 	else:
 		opp = 'user'
 
-	while count > 0:
-		deck.remove(opp, card)
-		deck.append(turn, card)
+	while count > 0:	#for however many of a particular card player had
+		deck.remove(opp, card)		#remove from opponent
+		deck.append(turn, card)		#give to player
 		count = count - 1
 
 	deck.checkForSet(turn)
 	time.sleep(1)
 		
-
+#Game play continues until a player runs out of cards or the deck is empty
 while len(deck.userhand) > 0 and len(deck.comphand) > 0 and len(deck.deck) > 0:
-	if turn == "user":
+	if turn == "user":	#users turn
 		print("\n")
-		deck.printHand()
+		deck.printHand()	#print the users hand
 		
+		#ask user to guess and process input
 		user_input = input("Which card would you like to ask me for?: ")
 		cardType = processInput(user_input)
 
-		if cardType == -1:
+		if cardType == -1:	#provide error message
 			print("Invalid input, please ask for a valid card (i.e. 2 - 10, J, Q, K, A)")
-		elif cardType == -2:
+		elif cardType == -2:	#provide error message
 			print("Invalid input, please type only a single card")
 		else:
 			card = adjustCard(cardType, user_input)
-			count = deck.comphand.count(card)
-			if count == 0:
+			count = deck.comphand.count(card)	#get number of times card appears in hand
+			if count == 0:	#not in hand
 				guessWrong(turn)
-				turn = "comp"
-			else:
+				turn = "comp"	#change turns
+			else:	#was in opponents hand
 				guessRight(turn, card, count, cardType)
-				turn = "user"
+				turn = "user"	#turn remains here
 
-	if turn == "comp":
+	if turn == "comp":	#computeres turn
 		print("\n")
-		card = random.choice(deck.comphand)
+		card = random.choice(deck.comphand)	#select random card from hand to request
 		cardType = processInput(str(card))
 		print("Do you have any " + str(card) + "s")
-		count = deck.userhand.count(card)
+		count = deck.userhand.count(card)	#number of times card appears in users hand
 		time.sleep(1)
 
-		if count == 0:
+		if count == 0:	#not in users hand
 			guessWrong(turn)
-			turn = "user"
+			turn = "user"	#change turns
 		else:
 			guessRight(turn, card, count, cardType)
-			turn = "comp"
+			turn = "comp"	#turn remains here
 
+#Once while loop terminates handle the end of the game
 print("\n")
 print("GAME OVER...OUT OF CARDS")
 numCompSets = len(deck.compsets)
@@ -131,6 +138,7 @@ numUserSets = len(deck.usersets)
 print("You made " + str(numUserSets) + " sets")
 print("I made " + str(numCompSets) + " sets")
 
+#determine winner based on number of sets
 if numCompSets > numUserSets:
 	print("I WIN!")
 elif numUserSets > numCompSets:
