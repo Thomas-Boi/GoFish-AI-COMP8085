f14b382334670addd3fcf7c4249d6369cdbbdd91
Sebastian Bejm <125sbejm@gmail.com>
2022-03-09 16:11:44 -0800

    Adding the evaluation functions

--- player/SearchAI.py
+++ player/SearchAI.py
@@ -1,53 +1,56 @@
 import math
+import random
 
 from player.Opponent import *
 from player.RandomAI import *
 
-infinity = math.inf
-
 class SearchAI(RandomAI):
     """
     An AI that makes moves based on a heuristic.
-    Uses the Minimax algorithm to select the best move
+    This is based on the minimax algorithm to select the best move
     """
-    def minimax(self, target_opponent) -> Move:
-        max_depth = 2
-        current_depth = 0
-        chosen_card, best_value = self.max_value(target_opponent, current_depth, max_depth)
-        return Move(self.name, target_opponent.name, chosen_card)
 
-    def max_value(self, target_opponent, current_depth: int, max_depth: int) -> [str, float]:
+    def search(self, target_opponent):
+        current_hand = self.hand
+        best_card = self.evaluate(current_hand, target_opponent)
+        return best_card
 
-        best_card = None
-        best_value = -infinity
+    def utility(self, asked_card: str, target_opponent: Opponent):
+        """ Return the utility of the card if card was found (+1), otherwise return 0 """
+        if target_opponent.has_card(asked_card):
+            print(f"HAS THE CARD {asked_card}?")
+            return target_opponent.hand[asked_card]
+        return 0
 
-        # temporarily added
-        cards = []
-        for card, amount in self.hand.items():
-            for i in range(amount):
-                cards.append(card)
-        return random.choice(cards), best_value
-
-    def min_value(self, target_opponent, current_depth: int, max_depth: int) -> [str, float]:
-
-        best_card = None
-        best_value = infinity
-
-    def utiliity(self) -> int:
-        pass
-
-    def evaluate(self, target_opponent) -> int:
+    def evaluate(self, current_hand, target_opponent: Opponent) -> str:
+        # THIS FUNCTION IS INCOMPLETE
         # the weight of cards should be based on whether there is more cards for a certain type
-        # Ex: if current player hand has 2,3,3,4,4,4 => the 4's would have more weight => look to opponent hand
+        # all cards regardless of their suit/color have the same value
+        utility_hand = current_hand.copy()
+        utility_hand = {k: v for k, v in sorted(utility_hand.items(), key=lambda item: item[1])}
 
-        # all cards regardless of their suit/color have the same value =>
-        # its how many of a card out of the total cards a player has affects the value
+        for card, amount in reversed(utility_hand.items()):
+            utility_hand[card] += self.utility(card, target_opponent)
+        print("after utility")
+        print(utility_hand)
 
-        return -1
+        best_val = max(utility_hand.values())
+        best_card = max(utility_hand, key=utility_hand.get)
+        print(best_card)
+
+        return best_card
+
 
     def make_move(self, other_players: Tuple[OppStat], deck_count: int) -> Move:
-        # pick a random player
-        target = random.choice(other_players)
+        best_moves = [] # collect best possible moves from all available opponents
+        for opp_name, opp in self.opponents.items():
+            best_card = self.search(opp)
+            best_moves.append(Move(self.name, opp_name, best_card))
 
-        # perform minimax to find the best card to choose for this move
-        return self.minimax(target)
+        if len(best_moves) > 0:
+            # temporary, will have to find the best with multiple opponents later
+            return best_moves[0]
+
+        # perform the search to find the best card to choose for this move
+        return super().make_move(other_players, deck_count)
+
