0e41e9ec2c1b8edcabc7d74e3acb8e8c08fd3898
Thomas Bui <thomasbui198@gmail.com>
2022-03-29 10:08:14 -0700

    Add default for opp amount when play

--- go_fish.py
+++ go_fish.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         action='store_true')
 
     argparser.add_argument("--amount", help="The amount of enemies the player is playing against. Must be in play mode for this to count. ",
-        type=int, choices=range(1, 4)) # only 1-3 opponents
+        type=int, choices=range(1, 4), default=1) # only 1-3 opponents
 
     argparser.add_argument("--type", help="The type of enemy. 1 for RandomAI, 2 for OppAwareAI, 3 for SearchAI.", 
         type=int, choices=range(1, 4), default=2) # default is a medium bot.
